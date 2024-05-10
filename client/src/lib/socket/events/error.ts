9d32c759ec14d284528719a52581f8d9865f6537
Aaron Ragudos <workemailaaronragudos@gmail.com>
2024-05-10 08:43:18 +0800

    handle error from server

--- client/src/lib/socket/events/error.ts
+++ client/src/lib/socket/events/error.ts
@@ -1,3 +1,21 @@
-export function handleError(data: Array<number>) {}
+import { STATE } from "../../../state";
+import { parseData } from "../../parser";
+import { toast } from "../../toast";
 
-export function handleConnectError(data: Array<number>) {}
+export function handleError(data: Array<number>) {
+    try {
+        toast.error(parseData(data, "string"));
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export function handleConnectError(data: Array<number>) {
+    try {
+        toast.error(parseData(data, "string"));
+
+        STATE.socket.ws?.close();
+    } catch (err) {
+        console.error(err);
+    }
+}
